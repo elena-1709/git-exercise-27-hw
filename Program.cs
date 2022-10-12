cb3480c403dffff9963d47bbfc65de663c406536
Elena <elena1709@yandex.ru>
2022-10-12 14:48:09 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите функцию и запустите ее, которая
+// принимает на вход число и выдаёт сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int A)
+{
+int sum = 0;
+
+    for (int i = 0; A != 0; i++)
+    {
+        sum += A % 10;
+        A /= 10;
+    }
+
+return sum; 
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+int result = Sum(A);
+Console.WriteLine($"Сумма цифр = {result}");
